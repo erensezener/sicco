5e70bcdaf5e99687f23001d0d9df339b4815958b
Eren Sezener <erensezener@gmail.com>
2016-04-23 17:53:58 +0200

    clean up examples

--- examples/dummy_config.py
+++
@@ -1,9 +0,0 @@
-import sicco_config
-import utils
-import config
-
-
-class DummyConfig(config.Config):
-    def __init__(self):
-        super().__init__()
-        self.echo_string = 'lolo'
--- examples/svm_experiment_example.py
+++ examples/svm_experiment_example.py
@@ -1,5 +1,20 @@
+import experiment_runner
+import config
 import experiment
 from sklearn import svm
+
+
+class SVMConfig(config.Config):
+    def __init__(self):
+        super().__init__()
+        self.X_train = [[0, 0], [1, 1]]
+        self.y_train = [0, 1]
+        self.X_test = [[2, 2]]
+        self.y_test = [1]
+        self.svm_C = 1.5
+
+    def get_data(self):
+        return (self.X_train, self.y_train, self.X_test, self.y_test)
 
 
 class SVMExperiment(experiment.Experiment):
@@ -18,3 +33,12 @@ class SVMExperiment(experiment.Experiment):
 
     def get_model_params(self):
         return {'classifier': self.clf}
+
+
+config0 = SVMConfig()
+config1 = SVMConfig()
+
+experiment = SVMExperiment()
+runner = experiment_runner.ExperimentRunner(experiment, [config0, config1])
+runner.run()
+runner.save_logs()
